494b961094674a06baf4d1dafeadbf8921b8d35c
hanjunhee90 <jhhan718@gmail.com>
2024-08-21 10:58:51 +0900

    Thread study
    스레드 공부한것

---
+++ Chef.java
@@ -0,0 +1,26 @@
+package ch13_thread.issac;
+
+public class Chef extends Thread{
+	private Issac issac = Issac.getntance();
+	private int count;
+	public EndCook endCook;
+	
+	public Chef(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public void run() {
+		for(int i = 0; i < count; i++) {
+			try {
+				Thread.sleep(1000); //1초에 1개의 토스트 생산
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			issac.makeToast();
+		}
+		endCook.endOfCook();
+	}
+	
+	
+}
